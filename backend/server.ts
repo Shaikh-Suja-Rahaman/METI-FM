d1d8d593ce07558588f7629ea450ba19702ca1b2
Shaikh-Suja-Rahaman <sujarahaman.dms.727@gmail.com>
2026-04-30 18:58:18 +0530

    basic ts server implementation on using node+express

---
+++ backend/server.ts
@@ -0,0 +1,26 @@
+import express from 'express';
+import type {Request, Response} from 'express'
+
+import dotenv from 'dotenv'
+
+
+dotenv.config()
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+app.use(express.json())
+
+app.get('/', (req:Request, res:Response)=>{
+  res.status(200).send("Hello wassup");
+})
+
+app.post('/', (req:Request, res:Response)=>{
+  const body = req.body;
+  res.status(200).send(body);
+})
+
+app.listen(PORT, ()=>{
+  console.log(`Server running on PORT ${PORT}`);
+})
+
+app.use()
